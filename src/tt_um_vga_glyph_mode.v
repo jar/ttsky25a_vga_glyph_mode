36fedb0922fd4be1d04f888e36b2e3979ce6e472
James Ross <james.a.ross@gmail.com>
2024-09-03 22:10:05 -0400

    added more color palettes

--- src/tt_um_vga_glyph_mode.v
+++ src/tt_um_vga_glyph_mode.v
@@ -35,12 +35,13 @@ module tt_um_vga_glyph_mode(
 	wire [6:0] x_mix = {xb[3], xb[1], xb[4], xb[1], xb[6], xb[0], xb[2]};
 	wire [2:0] g_x = pix_x[2:0];
 	wire [5:0] yb;
-	wire [9:0] g_y9 = pix_y - {yb, 3'b000} - {1'b0, yb, 2'b00};
-	wire [3:0] g_y = g_y9[3:0];
+	wire [5:0] g_unused;
+	wire [3:0] g_y;
+	assign {g_unused, g_y} = pix_y - {yb, 3'b000} - {1'b0, yb, 2'b00};
 	wire hl;
 
 	// Suppress unused signals warning
-	wire _unused_ok = &{ena, ui_in, uio_in};
+	wire _unused_ok = &{ena, ui_in[7:2], uio_in};
 
 	reg [9:0] counter;
 
@@ -69,8 +70,10 @@ module tt_um_vga_glyph_mode(
 		.out(yb)
 	);
 
-	wire [6:0] r = x[6:0] >> 3;
-	wire [5:0] glyph_index = {xb[2] ^ yb[0], xb[0] ^ yb[1], xb[1] ^ yb[2], xb[4] ^ yb[3], xb[3] ^ yb[4]} + {1'b0, xb[5] ^ yb[5], xb[6] ^ yb[0], xb[0] ^ yb[1], xb[1] ^ yb[2]} + r[5:0];
+	wire [5:0] r = x[6:1] >> 2;
+	wire [5:0] glyph_index = {xb[2] ^ yb[0], xb[0] ^ yb[1], xb[1] ^ yb[2], xb[4] ^ yb[3], xb[3] ^ yb[4]} // [0,31]
+		+ {1'b0, xb[5] ^ yb[5], xb[6] ^ yb[0], xb[0] ^ yb[1], xb[1] ^ yb[2]} // [0,15]
+		+ r; // [0,7]
 
 	wire [1:0] a = xb[1:0];
 	wire [3:0] b = xb[5:2];
@@ -85,12 +88,13 @@ module tt_um_vga_glyph_mode(
 	wire [6:0] f = v[6:0] & e;
 	wire [6:0] x = v[6:0] >> a;
 	wire [2:0] y = x[2:0] ^ 3'b111;
+	wire [5:0] black = 6'b000000;
 
-	wire [5:0] z = (((v[2:0] & 3'b111) == 3'b000) & y == 7) ? 6'b111111 : palette[y];
+	wire [5:0] z = (((v[2:0] & 3'b111) == 3'b000) & y == 7) ? 6'b111111 : palette[ui_in[1:0]][y];
 
-	wire [5:0] color = ((f != 7'd0) | n) ? palette[0] : z;
+	wire [5:0] color = ((f != 7'd0) | n) ? black : z;
 
-	assign RGB = (video_active & hl) ? color : palette[0];
+	assign RGB = (video_active & hl) ? color : black;
 	
 	always @(posedge vsync) begin
 		if (~rst_n) begin
@@ -101,16 +105,39 @@ module tt_um_vga_glyph_mode(
 	end
 
 	// color palette (RRGGBB)
-	reg [5:0] palette[0:7]; // RRGGBB
+	reg [5:0] palette[3:0][7:0];
 	initial begin
-		palette[0] = 6'b000000;
-		palette[1] = 6'b000100;
-		palette[2] = 6'b001000;
-		palette[3] = 6'b001100;
-		palette[4] = 6'b001101; //6'b001100;
-		palette[5] = 6'b011101; //6'b011100;
-		palette[6] = 6'b011110; //6'b011101;
-		palette[7] = 6'b101110; //6'b101101;
+		palette[0][0] = 6'b000000; // green (default)
+		palette[0][1] = 6'b000100;
+		palette[0][2] = 6'b001000;
+		palette[0][3] = 6'b001100;
+		palette[0][4] = 6'b001101;
+		palette[0][5] = 6'b011101;
+		palette[0][6] = 6'b011110;
+		palette[0][7] = 6'b101110;
+		palette[1][0] = 6'b000000; // red
+		palette[1][1] = 6'b010000;
+		palette[1][2] = 6'b100000;
+		palette[1][3] = 6'b110000;
+		palette[1][4] = 6'b110001;
+		palette[1][5] = 6'b110101;
+		palette[1][6] = 6'b110110;
+		palette[1][7] = 6'b111010;
+		palette[2][0] = 6'b000000; // blue
+		palette[2][1] = 6'b000001;
+		palette[2][2] = 6'b000010;
+		palette[2][3] = 6'b000011;
+		palette[2][4] = 6'b000111;
+		palette[2][5] = 6'b010111;
+		palette[2][6] = 6'b011011;
+		palette[2][7] = 6'b101011;
+		palette[3][0] = 6'b000000; // pride
+		palette[3][1] = 6'b110000;
+		palette[3][2] = 6'b111000;
+		palette[3][3] = 6'b111100;
+		palette[3][4] = 6'b001000;
+		palette[3][5] = 6'b000111;
+		palette[3][6] = 6'b100010;
+		palette[3][7] = 6'b110011;
 	end
-
 endmodule
