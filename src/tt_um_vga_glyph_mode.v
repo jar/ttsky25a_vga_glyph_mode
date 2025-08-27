42c73cc4f63f2cfe6200c331e53a5f8792b21e48
James Ross <james.a.ross@gmail.com>
2025-08-27 00:55:21 -0400

    reorganized top and moved pallete to rom

--- src/tt_um_vga_glyph_mode.v
+++ src/tt_um_vga_glyph_mode.v
@@ -58,10 +58,19 @@ module tt_um_vga_glyph_mode(
 
 	// glyphs
 	glyphs_rom glyphs(
-			.c(glyph_index),
-			.y(g_y),
-			.x(g_x),
-			.pixel(hl)
+		.c(glyph_index),
+		.y(g_y),
+		.x(g_x),
+		.pixel(hl)
+	);
+
+	// palette
+	wire [5:0] glyph_color;
+	palette_rom palettes(
+		.cid(y),
+		.pid(ui_in[1:0]),
+		//.pid(2'b00),
+		.color(glyph_color)
 	);
 
 	// division by 3
@@ -70,27 +79,29 @@ module tt_um_vga_glyph_mode(
 		.out(yb)
 	);
 
-	wire [5:0] r = x[6:1] >> 2;
+	// there are 48 glyphs [0,47], so we compute a value in that range
 	wire [5:0] glyph_index = {xb[2] ^ yb[0], xb[0] ^ yb[1], xb[1] ^ yb[2], xb[4] ^ yb[3], xb[3] ^ yb[4]} // [0,31]
 		+ {1'b0, xb[5] ^ yb[5], xb[6] ^ yb[0], xb[0] ^ yb[1], xb[1] ^ yb[2]} // [0,15]
-		+ r; // [0,7]
+		+ {2'b00, x[6:3]}; // [0,7]
 
 	wire [1:0] a = xb[1:0];
 	wire [3:0] b = xb[5:2];
 	wire [2:0] d = xb[3:2] + 2'd3;
 
-	wire s = xb[0] ^ xb[1] ^ xb[2] ^ xb[3] ^ xb[4] ^ xb[5] ^ xb[6];
-	wire n = xb[1] ^ xb[3] ^ xb[5];
+	// column features
+	wire s = xb[0] ^ xb[1] ^ xb[2] ^ xb[3] ^ xb[4] ^ xb[5] ^ xb[6]; // speed
+	wire n = xb[1] ^ xb[3] ^ xb[5]; // on or off
 
 	wire [6:0] v = (counter[9:3] << s) - yb - x_mix;
 	wire [3:0] c = {2'b00, a} + d;
 	wire [6:0] e = {3'b000, b} << c;
-	wire [6:0] f = v[6:0] & e;
-	wire [6:0] x = v[6:0] >> a;
+	wire [6:0] f = v & e;
+	wire [6:0] x = v >> a;
 	wire [2:0] y = x[2:0] ^ 3'b111;
 	wire [5:0] black = 6'b000000;
+	wire [5:0] white = 6'b111111;
 
-	wire [5:0] z = (((v[2:0] & 3'b111) == 3'b000) & y == 7) ? 6'b111111 : palette[ui_in[1:0]][y];
+	wire [5:0] z = (((v[2:0] & 3'b111) == 3'b000) & y == 7) ? white : glyph_color;
 
 	wire [5:0] color = ((f != 7'd0) | n) ? black : z;
 
@@ -104,40 +115,4 @@ module tt_um_vga_glyph_mode(
 		end
 	end
 
-	// color palette (RRGGBB)
-	reg [5:0] palette[3:0][7:0];
-	initial begin
-		palette[0][0] = 6'b000000; // green (default)
-		palette[0][1] = 6'b000100;
-		palette[0][2] = 6'b001000;
-		palette[0][3] = 6'b001100;
-		palette[0][4] = 6'b001101;
-		palette[0][5] = 6'b011101;
-		palette[0][6] = 6'b011110;
-		palette[0][7] = 6'b101110;
-		palette[1][0] = 6'b000000; // red
-		palette[1][1] = 6'b010000;
-		palette[1][2] = 6'b100000;
-		palette[1][3] = 6'b110000;
-		palette[1][4] = 6'b110001;
-		palette[1][5] = 6'b110101;
-		palette[1][6] = 6'b110110;
-		palette[1][7] = 6'b111010;
-		palette[2][0] = 6'b000000; // blue
-		palette[2][1] = 6'b000001;
-		palette[2][2] = 6'b000010;
-		palette[2][3] = 6'b000011;
-		palette[2][4] = 6'b000111;
-		palette[2][5] = 6'b010111;
-		palette[2][6] = 6'b011011;
-		palette[2][7] = 6'b101011;
-		palette[3][0] = 6'b000000; // pride
-		palette[3][1] = 6'b110000;
-		palette[3][2] = 6'b111000;
-		palette[3][3] = 6'b111100;
-		palette[3][4] = 6'b001000;
-		palette[3][5] = 6'b000111;
-		palette[3][6] = 6'b100010;
-		palette[3][7] = 6'b110011;
-	end
 endmodule
